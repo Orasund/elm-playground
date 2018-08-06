1deb530ef78cf839d9c16d791c9dd850bb6ae2ee
Orasund <orasund@gmail.com>
2018-08-06 21:21:44 +0200

    recreated clock

--- src/Clock.elm
+++ src/Clock.elm
@@ -2,17 +2,13 @@ module Clock exposing (main)
 
 --import Html.Attributes exposing (..)
 
-import Bootstrap.CDN as CDN
-import Bootstrap.Grid as Grid
-import Html exposing (Html, div, text)
-import Html.Attributes exposing (style)
-import Refined exposing (Refined, refine)
-import Svg exposing (circle, line, svg)
-import Svg.Attributes exposing (cx, cy, fill, r, stroke, viewBox, x1, x2, y1, y2)
+import Css
+import Date exposing (Date)
+import Html.Styled as Html exposing (Html)
+import Html.Styled.Attributes exposing (css)
+import Task
 import Time exposing (Time)
-import Time.TimeZones exposing (europe_vienna)
-import Time.ZonedDateTime exposing (ZonedDateTime, fromTimestamp, hour, millisecond, minute, second)
-import Window exposing (resizes)
+import Window
 
 
 main : Program Never Model Msg
@@ -30,39 +26,31 @@ main =
 
 
 type alias Model =
-    { time : Refined Time
-    , log : List String
-    , size : Window.Size
+    { time : Date
+    , width : Int
+    , height : Int
     }
 
 
 init : ( Model, Cmd Msg )
 init =
     let
-        log : Result String (List String)
-        log =
-            Ok []
-
-        time : Result String (Refined Time)
+        time : Date
         time =
-            refine 0 (\_ -> True)
+            Date.fromTime 0
 
-        size : Result String Window.Size
-        size =
-            Ok { width = 0, height = 0 }
+        width : Int
+        width =
+            0
 
-        model : Model
-        model =
-            case
-                Result.map3 Model time log size
-            of
-                Ok a ->
-                    a
-
-                _ ->
-                    Debug.crash "invalid initial state: "
+        height : Int
+        height =
+            0
     in
-    model ! []
+    { time = time, width = width, height = height }
+        ! [ Task.perform Resize Window.size
+          , Task.perform Tick Time.now
+          ]
 
 
 
@@ -71,29 +59,17 @@ init =
 
 type Msg
     = Tick Time
-    | Size Window.Size
+    | Resize Window.Size
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Tick newTime ->
-            let
-                new_model =
-                    case
-                        model.time
-                            |> Refined.set newTime
-                    of
-                        Ok a ->
-                            { model | time = a }
+        Tick time ->
+            { model | time = Date.fromTime time } ! []
 
-                        Err str ->
-                            { model | log = str :: model.log }
-            in
-            new_model ! []
-
-        Size s ->
-            { model | size = s } ! []
+        Resize { height, width } ->
+            { model | height = height, width = width } ! []
 
 
 
@@ -104,40 +80,69 @@ subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.batch
         [ Time.every Time.second Tick
-        , resizes (identity Size)
+        , Window.resizes Resize
         ]
 
 
 
 -- VIEW
 
+color : Int -> Css.Color
+color hour =
+    case hour of
+        12 -> Css.rgb 23 240 240
+        11 -> Css.rgb 255 178 25
+        10 -> Css.rgb 95 49 243
+        9 -> Css.rgb 255 255 25
+        8 -> Css.rgb 246 24 174
+        7 -> Css.rgb 24 248 24
+        6 -> Css.rgb 255 129 25
+        5 -> Css.rgb 46 106 243
+        4 -> Css.rgb 255 216 25
+        3 -> Css.rgb 163 36 243
+        2 -> Css.rgb 177 253 25
+        _ -> Css.rgb 255 25 25
+
+
+circle : Int -> { height : Int, width : Int } -> List Css.Style -> List (Html Msg) -> Html Msg
+circle size { height, width } styleList htmlList =
+    Html.div
+        [ css
+            ([ Css.textAlign Css.center
+             , Css.position Css.fixed
+             , Css.top <| Css.px <| toFloat <| (height - size) // 2
+             , Css.left <| Css.px <| toFloat <| (width - size) // 2
+             , Css.width <| Css.px <| toFloat <| size
+             , Css.height <| Css.px <| toFloat <| size
+             , Css.borderRadius <| Css.px <| toFloat <| size // 2
+             ]
+                |> List.append styleList
+            )
+        ]
+        htmlList
+
 
 view : Model -> Html Msg
-view model =
+view ({ time, height, width } as model) =
     let
-        time : ZonedDateTime
-        time =
-            Refined.get model.time
-                |> fromTimestamp (europe_vienna ())
+        size =
+            max height width
 
         hours =
-            hour time
+            Date.hour time
 
         minutes =
-            minute time
+            Date.minute time
 
         seconds =
-            second time
+            Date.second time
 
         milli =
-            millisecond time
-
-        log : List String
-        log =
-            model.log
+            Date.millisecond time
 
         angle =
-            Refined.get model.time
+            time
+                |> Date.toTime
                 |> Time.inMinutes
                 |> turns
 
@@ -147,30 +152,26 @@ view model =
         handY =
             toString (50 + 40 * sin angle)
     in
-    Grid.container []
-        -- Responsive fixed width container
-        [ CDN.stylesheet -- Inlined Bootstrap CSS for use with reactor
-        , Grid.row []
-            [ Grid.col []
-                [ text (toString log) ]
-            , Grid.col []
-                [ div
-                    [ style
-                        [ ( "size", "1000px" )
-                        , ( "background", "red" )
-                        ]
-                    ]
-                    [ text "test" ]
-                ]
-            , Grid.col []
-                [ text (toString hours ++ ":" ++ toString minutes)
-                ]
-            , Grid.col []
-                [ svg [ viewBox "0 0 100 100", Svg.Attributes.width "300px" ]
-                    [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-                    , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
-                    ]
-                ]
-            , Grid.col [] [ text (toString [ hours, minutes, seconds, milli ]) ]
+    Html.div
+        [ css
+            [ Css.width <| Css.pct <| 100
+            , Css.height <| Css.pct <| 100
+            , Css.backgroundColor <| Css.rgb 0 0 0
             ]
+        ]
+        [ circle
+            size
+            { height = height, width = width }
+            [ Css.backgroundColor <| Css.rgb 0 0 0
+            , Css.border3 (Css.px <| 1) Css.solid (Css.rgb 63 63 63)
+            ]
+            []
+        , circle (size // 2 + (size * minutes) // (60 * 2))
+            { height = height, width = width }
+            [ Css.backgroundColor <| Css.rgb 255 255 255 ]
+            []
+        , circle (size // 2)
+            { height = height, width = width }
+            [ Css.backgroundColor <| color hours]
+            [ Html.text (toString hours ++ ":" ++ toString minutes) ]
         ]
